8644b203bc9cc238da72d59d5ed0ef20a6502609
Kevin Liebholz <kevin@liebholz.eu>
2024-04-09 16:50:08 +0200

    Initial Commit

---
+++ personal_knowledge_base-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personal_knowledge_base-ios
+//
+//  Created by Kevin Liebholz on 09.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
