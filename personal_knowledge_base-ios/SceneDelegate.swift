8941fc6cf4b445502ada4bde71e08976511f41b7
Kevin Liebholz <kevin@liebholz.eu>
2024-04-11 16:11:49 +0200

    first working connection

--- personal_knowledge_base-ios/SceneDelegate.swift
+++ personal_knowledge_base-ios/SceneDelegate.swift
@@ -11,10 +11,16 @@ import Turbo
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    // A session in Turbo native is the object that visits screens, pops them onto the hierachy, adapter that hooks into the JS for Turbo.js,
-    private let session = Session()
     private let navigationController = UINavigationController()
     
+    // A session in Turbo native is the object that visits screens, pops them onto the hierachy, adapter that hooks into the JS for Turbo.js,
+    private lazy var session: Session = {
+        let session = Session()
+        session.delegate = self // `self` == SceneDelegate
+        return session
+    }()
+    
+    // App launches
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
@@ -24,11 +30,35 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         visit()
     }
     
+    // a VisitableViewController doesn't do anything on its own, it just manages the web view. So, we still need to actually visit the URL. We do that with a session
+    // App launches
     private func visit() {
         let url = URL(string: "http://localhost:3000")!
-        // a VisitableViewController doesn't do anything on its own, it just manages the web view. So, we still need to actually visit the URL. We do that with a session
         let controller = VisitableViewController(url: url)
         session.visit(controller, action: .advance)
+        
+        // push contoller (aka screen) onto the stack
         navigationController.pushViewController(controller, animated: true)
     }
+}
+
+// without the Delegate, the framework cannot let us know when a new screen/link is pushed
+
+// Tell SceneDelegate that it conforms to the SessionDelegate protocol
+extension SceneDelegate: SessionDelegate {
+    func session(_ session: Session, didProposeVisit proposal: VisitProposal) {
+        let controller = VisitableViewController(url: proposal.url)
+        session.visit(controller, options: proposal.options) // options: type of action (advance, replace...), maintain cache, ...
+        navigationController.pushViewController(controller, animated: true)
+    }
+    
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, error: Error) {
+        // TODO: handle errors e.g. 400.
+    }
+    
+    func sessionWebViewProcessDidTerminate(_ session: Session) {
+        // TODO: handle dead web view.
+    }
+    
+    
 }
